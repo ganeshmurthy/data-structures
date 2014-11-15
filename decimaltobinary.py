0d5bfa3ed83ae558768d136d9c4bce8790d32f38
Ganesh Murthy <ganesh_boston@yahoo.com>
2014-11-15 18:30:55 -0500

    Added some test cases

--- decimaltobinary.py
+++ decimaltobinary.py
@@ -2,19 +2,28 @@ def decimal_to_binary(decimal_number):
     """
     Converts decimal numbers to binary representation 
     """
-    
     if decimal_number < 0:
         return 'No decimal representation for negative numbers'
-
-    if decimal_number == 0:
+    elif decimal_number == 0:
         return ''
 
     if decimal_number:
         quotient = decimal_number/2
         remainder = decimal_number % 2
         return str(decimal_to_binary(quotient)) + str(remainder)
-    else:
-        print 'Provide a valid decimal number'
 
 if(__name__=="__main__"):
+    #1010
+    print decimal_to_binary(10)
+
+    #1111
+    print decimal_to_binary(15)
+
+    #1100
+    print decimal_to_binary(12)
+
+    #10100
     print decimal_to_binary(20)
+
+    #11001
+    print decimal_to_binary(25)
